225af792efb79495a7de755b94dc0a69638276f6
Reuzel <joost_reuzel@hotmail.com>
2014-04-16 01:20:01 +0200

    getting rid of boxing in projections

--- CqlSharp.Linq/Query/ProjectorBuilder.cs
+++ CqlSharp.Linq/Query/ProjectorBuilder.cs
@@ -52,23 +52,135 @@ namespace CqlSharp.Linq.Query
         /// <returns></returns>
         public override Expression VisitSelector(SelectorExpression selector)
         {
-            var value = Expression.MakeIndex(_reader, Indexer, new[] { Expression.Constant(selector.Ordinal) });
+            Expression value = null;
 
             //check if it is a token (of which we don't know it's type)
             if (selector.Type == typeof(CqlToken))
-                return Expression.New(TokenConstructor, value);
+                return Expression.New(TokenConstructor,
+                                      Expression.MakeIndex(_reader, Indexer, new[] { Expression.Constant(selector.Ordinal) }));
 
-            //check if it is a value type (and change null values to corresponding default values)
-            if (selector.Type.IsValueType)
+            switch (selector.Type.ToCqlType())
             {
-                return Expression.Condition(
-                    Expression.Equal(value, Expression.Constant(null)),
-                    Expression.Default(selector.Type),
-                    Expression.Convert(value, selector.Type));
+                case CqlType.Set:
+                    value = Expression.Call(_reader,
+                                            "GetSet",
+                                            new[] { TypeSystem.GetElementType(selector.Type) },
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.List:
+                    value = Expression.Call(_reader,
+                                            "GetList",
+                                            selector.Type.GetGenericArguments(),
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Map:
+                    value = Expression.Call(_reader,
+                                            "GetDictionary",
+                                            selector.Type.GetGenericArguments(),
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Varchar:
+                case CqlType.Text:
+                case CqlType.Ascii:
+                    value = Expression.Call(_reader,
+                                            "GetString",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Int:
+                    value = Expression.Call(_reader,
+                                            "GetInt32",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Counter:
+                case CqlType.Bigint:
+                    value = Expression.Call(_reader,
+                                            "GetInt64",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Varint:
+                    value = Expression.Call(_reader,
+                                            "GetBigInteger",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Blob:
+                    value = Expression.Call(_reader,
+                                            "GetBytes",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Boolean:
+                    value = Expression.Call(_reader,
+                                            "GetBoolean",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Decimal:
+                    value = Expression.Call(_reader,
+                                            "GetDecimal",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Double:
+                    value = Expression.Call(_reader,
+                                            "GetDouble",
+                                            null,
+                                            Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Float:
+                    value = Expression.Call(_reader,
+                                           "GetFloat",
+                                           null,
+                                           Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Inet:
+                    value = Expression.Call(_reader,
+                                           "GetIPAddress",
+                                           null,
+                                           Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Timestamp:
+                    value = Expression.Call(_reader,
+                                           "GetDateTime",
+                                           null,
+                                           Expression.Constant(selector.Ordinal));
+                    break;
+
+                case CqlType.Uuid:
+                case CqlType.Timeuuid:
+                    value = Expression.Call(_reader,
+                                           "GetGuid",
+                                           null,
+                                           Expression.Constant(selector.Ordinal));
+                    break;
+
+                default:
+                    value = Expression.Convert(
+                        Expression.MakeIndex(_reader, Indexer, new[] { Expression.Constant(selector.Ordinal) }),
+                        selector.Type);
+                    break;
+
             }
 
-            //any other class value
-            return Expression.Convert(value, selector.Type);
+
+
+            return value;
         }
 
         /// <summary>
